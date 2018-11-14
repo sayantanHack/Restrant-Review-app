ae78b9ddc8397bd5a33f3e35c6530d7cf1d87e8a
Sayantan <sayantandatta97@gmail.com>
2018-11-15 01:16:59 +0530

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -59,7 +59,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img';
-  image.alt = restaurant.name;
+  image.alt = restaurant.name,"is great & famous in Bengal";
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
