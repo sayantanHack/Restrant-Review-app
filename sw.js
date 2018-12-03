5df146dd85f156a06e9cbaf6da9462ecb31cc78d
Sayantan <sayantandatta97@gmail.com>
2018-12-03 01:13:52 -0500

    adding new fetchings

--- sw.js
+++ sw.js
@@ -45,10 +45,16 @@ self.addEventListener("activate", event => {
 })
 
 // Fetch service workers
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", event => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then(response => {
+                if (response) {
+                    // console.log("[ServiceWorker] Found in cache ", event.request.url);
+                    return response;
+                }
+                return fetch(event.request);
+            })
+        );
+    }
 });
